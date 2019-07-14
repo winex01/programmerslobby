ccc4487300ada5a3c5a2e88124133b1c39927ec9
winex01 <winnie131212592@gmail.com>
2019-07-14 22:38:01 +0800

    fix temporary template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -137,8 +137,65 @@
                 </div>
 
            </div>
+
+           <div class="row">
+
+                <div class="col-md-4">
+                    <div class="single-blog">
+                        <p class="blog-meta">By Admin <span>September 24, 2018</span></p>
+                        <img style="width: 100%; height: 250px;" src="/images/img1.jpg" class="img-responsive" alt="Image">
+                        <h2><a href="#">Learn How To Make a Website</a></h2>
+                        <p class="blog-text">Like this video and ask your questions in comment sections, don't forget to Subscribe Easy Tutorials YouTube Channel to watch more videos...</p>
+                        <p><a class="read-more-btn" href="#">Read More</a>
+                            <span><i class="fa fa-thumbs-o-up"></i>7 People like, <i class="fa fa-comments-o"></i> 3.. 
+                            </span>
+                        </p>
+                    </div>  
+                </div>
+
+                <div class="col-md-4">
+                    <div class="single-blog">
+                        <p class="blog-meta">By Winnie Damayo <span>October 04, 2018</span></p>
+                        <img style="width: 100%; height: 250px;" src="/images/img2.jpg" class="img-responsive" alt="Image">
+                        <h2><a href="#">Learn How To Make a Website</a></h2>
+                        <p class="blog-text">Like this video and ask your questions in comment sections, don't forget to Subscribe Easy Tutorials YouTube Channel to watch more videos...</p>
+                        <p><a class="read-more-btn" href="#">Read More</a>
+                            <span><i class="fa fa-thumbs-o-up"></i>7 People like, <i class="fa fa-comments-o"></i> 3.. 
+                            </span>
+                        </p>
+                    </div>  
+                </div>
+
+                <div class="col-md-4">
+                    <div class="single-blog">
+                        <p class="blog-meta">By Jane Dough <span>July 18, 2018</span></p>
+                        <img style="width: 100%; height: 250px;" src="/images/img3.jpg" class="img-responsive" alt="Image">
+                        <h2><a href="#">Learn How To Make a Website</a></h2>
+                        <p class="blog-text">Like this video and ask your questions in comment sections, don't forget to Subscribe Easy Tutorials YouTube Channel to watch more videos...</p> 
+                        <p><a class="read-more-btn" href="#">Read More</a>
+                            <span><i class="fa fa-thumbs-o-up"></i>7 People like, <i class="fa fa-comments-o"></i> 3.. 
+                            </span>
+                        </p>
+                    </div>  
+                </div>
+                
+                <div class="ml-3 mt-3">
+                    <nav aria-label="Page navigation example">
+                      <ul class="pagination justify-content-center">
+                        <li class="page-item disabled">
+                          <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                          <a class="page-link" href="#">Next</a>
+                        </li>
+                      </ul>
+                    </nav>
+                </div>
        </div>
-       
+
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
