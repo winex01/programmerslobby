d748aa933f521b5a40bbd9b613cf4c089c97deea
winnie <winnie131212592@gmail.com>
2019-07-16 22:28:07 +0800

    remove unwated comment and white space

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,17 +67,6 @@
     </head>
     <body>
 
-        {{-- <nav class="navbar navbar-expand-sm navbar-light bg-light fixed-top">
-          <a class="navbar-brand text-muted" href="#">ProgrammersLobby.com</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-            
-          </div>
-        </nav> --}}
-
-
         <nav class="navbar py-0 navbar-expand-sm navbar-light bg-light fixed-top py-md-0">
             <a class="navbar-brand text-muted" href="#">ProgrammersLobby.com</a>
             <button class="navbar-toggler mt-1" type="button" data-toggle="collapse" data-target="#navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -86,11 +75,12 @@
             <div class="navbar-collapse collapse" id="navbarNav">
                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li class="nav-item active"><a class="nav-link" href="#">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Programming</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Web</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Language</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">Meme</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">Submit Code</a></li>
-                  {{-- <li class="nav-item"></li> --}}
-                  {{-- <li class="nav-item"></li> --}}
                     
                 </ul>
 
@@ -100,13 +90,8 @@
 
                 <a class="ml-2 text-muted" href="#">Login</a>
                 <a class="ml-2 btn btn-outline-primary btn-sm my-2 my-sm-0" href="#">Sign up</a>
-              {{-- <button class="btn btn-outline-primary btn-sm my-2 my-sm-0 ml-1" type="submit">Search</button> --}}
-                
             </div>
-
         </nav>
-
-
 
        <div class="container mt-5">
            <h1 class="text-center">Get Source Code For Free</h1>
