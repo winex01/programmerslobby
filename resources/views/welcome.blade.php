8cf9d1a4c0a3f774bffb0276cbfb81838cd5f5de
winnie <winnie131212592@gmail.com>
2019-07-19 23:04:20 +0800

    fix mix all.css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <title>Laravel</title>
         <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        
+        <link rel="stylesheet" type="text/css" href="{{ mix('/css/all.css') }}">
      
     </head>
     <body>
