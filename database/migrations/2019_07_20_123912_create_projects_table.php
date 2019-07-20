27527b16d11c6bcfcba02afc40bdc8f5e73ccbba
winnie <winnie131212592@gmail.com>
2019-07-20 20:50:02 +0800

    add project migraiton, model and controller

---
+++ database/migrations/2019_07_20_123912_create_projects_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            //content
+            $table->string('title');
+            $table->text('body');
+            $table->string('image')->nullable();
+            $table->string('slug')->unique();
+            
+            //seo
+            $table->string('seo_title')->nullable();
+            $table->text('excerpt')->nullable(); //can be meta desc
+            $table->text('meta_description')->nullable();
+            $table->text('meta_keywords')->nullable();
+            $table->enum('status', ['PUBLISHED', 'DRAFT', 'PENDING'])->default('DRAFT');
+            $table->boolean('featured')->default(0);
+
+            $table->integer('author_id');
+            $table->integer('category_id')->nullable();
+
+            $table->foreign('author_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
