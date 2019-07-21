8f5d4549351e6aaa1fbfd84d17ad984940a28f8a
winnie <winnie131212592@gmail.com>
2019-07-21 16:45:12 +0800

    use bootstrap tooltip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
                     <div class="single-blog">
                         <p class="blog-meta">By {{ $project->author->name }} <span>{{ $project->created_at->diffForHumans() }}</span></p>
                         <img src="{{ Voyager::image($project->thumbnail('cropped', 'image')) }}" class="img-responsive" alt="Image">
-                        <h2><a href="#" title="{{ $project->title }}">{{ str_limit($project->title, 40) }}</a></h2>
+                        <h2><a href="#" data-toggle="tooltip" title="{{ $project->title }}">{{ str_limit($project->title, 40) }}</a></h2>
                         <p class="blog-text">{!! (str_limit(strip_tags($project->body), 190)) !!}</p>
                         <p><a class="read-more-btn" href="#">Read More</a>
                             <span><i class="fa fa-heart"></i> 7 People like, <i class="fa fa-comments-o"></i> 3.. 
@@ -33,3 +33,11 @@
         {{ $projects->links() }}
     </div>
 @endsection
+
+@push('js')
+<script>
+    $(document).ready(function(){
+      $('[data-toggle="tooltip"]').tooltip(); 
+    });
+</script>
+@endpush
