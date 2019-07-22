6364773177d0e1c5aa234026c368d02fe53825dd
winnie <winnie131212592@gmail.com>
2019-07-22 21:08:23 +0800

    fix route

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,4 @@ Route::group(['prefix' => 'admin'], function () {
 });
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('dashboard');
