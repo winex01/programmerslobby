fefa386dcb0eea74b971f0bac0a5cf8e90f18769
winnie <winnie131212592@gmail.com>
2019-07-27 00:17:18 +0800

    WIP single show project

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -7,17 +7,34 @@
 			
 			<img src="{{ Voyager::image($project->thumbnail('medium', 'image')) }}" class="img-responsive" alt="{{ $project->title }}">
 			
-			<br>
-			<h1>{{ $project->title }}</h1>
+			<div class="card mt-2">
+			  <div class="card-body">
+				<h4>{{ $project->title }}</h4>
+			  </div>
+			</div>
 			
-			{!! $project->body !!}
+
+			<div class="card mt-1">
+			  <div class="card-body">
+				<strong>Description:</strong>
+				{!! $project->body !!}
+			  </div>
+			</div>
 
 		</div>
-		<div class="col-md-4"></div>
+		<div class="col-md-4">
+			
+			<ul class="list-group">
+			  <li class="list-group-item">Cras justo odio</li>
+			  <li class="list-group-item">Dapibus ac facilisis in</li>
+			  <li class="list-group-item">Morbi leo risus</li>
+			  <li class="list-group-item">Porta ac consectetur ac</li>
+			  <li class="list-group-item">Vestibulum at eros</li>
+			</ul>
+
+		</div>
 	</div>
 
-	<div></div>
-
-
+	<br>
 
 @endsection
