8e728d33b44f4e2fbeb70701a8392405530442ce
winnie <winnie131212592@gmail.com>
2019-07-28 10:58:18 +0800

    fix widget buttons in links

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -40,25 +40,27 @@
 			@if($project->hasCodeLink)
 				<div class="card border-0 shadow mb-4 d-none d-lg-block">
 					<div class="card-body">
-						<a target="_blank" href="https://github.com/BlackrockDigital/startbootstrap-clean-blog/archive/gh-pages.zip" class="btn btn-block btn-danger mb-3 py-3">
-							<i class="fa fa-download" aria-hidden="true"></i> <span>{{ __('Free Download') }}</span>
-						</a>
-						<hr>
+						@if($project->download_link)
+							<a target="_blank" href="{{ $project->download_link }}" class="btn btn-block btn-danger mb-3 py-3">
+								<i class="fa fa-download" aria-hidden="true"></i> <span>{{ __('Free Download') }}</span>
+							</a>
+							<hr>
+						@endif
 
 						@if($project->gitlab_link)
-							<a href="/previews/clean-blog/" class="btn btn-block btn-outline-primary mb-3">
+							<a target="_blank" href="{{ $project->gitlab_link }}" class="btn btn-block btn-outline-primary mb-3">
 								<i class="fa fa-gitlab" aria-hidden="true"></i></i></i> {{ __('View on GitLab') }}
 							</a>
 						@endif
 
 						@if($project->github_link)
-							<a href="https://github.com/BlackrockDigital/startbootstrap-clean-blog" class="btn btn-block btn-outline-dark">
+							<a target="_blank" href="{{ $project->github_link }}" class="btn btn-block btn-outline-dark">
 								<i class="fa fa-github" aria-hidden="true"></i> {{ __('View on GitHub') }}
 							</a>
 						@endif
 
 						@if($project->sourcecode_link)
-							<a href="https://github.com/BlackrockDigital/startbootstrap-clean-blog" class="btn btn-block btn-outline-success">
+							<a target="_blank" href="{{ $project->sourcecode_link }}" class="btn btn-block btn-outline-success">
 								<i class="fa fa-code" aria-hidden="true"></i> {{ __('View SourceCode') }}
 							</a>
 						@endif
