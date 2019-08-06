7c5825ee2607f00700ccc16937d10cb903150f02
winnie <winnie131212592@gmail.com>
2019-08-06 12:51:45 +0800

    add custom quotes programming

--- resources/views/layouts/inspire.blade.php
+++ resources/views/layouts/inspire.blade.php
@@ -1,5 +1,5 @@
 <div class="alert alert-primary alert-dismissible fade show" role="alert">
-  <strong>Inspire!</strong> {{ \Illuminate\Foundation\Inspiring::quote() }}
+  <strong>Inspire!</strong> {{ \App\Quotes\Programming::quote() }}
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
