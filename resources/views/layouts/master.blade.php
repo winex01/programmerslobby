cdd87015a360fe84172677362a7587e63f536982
winnie <winnie131212592@gmail.com>
2019-08-10 11:16:52 +0800

    conver and use node for toastr package

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,11 +16,10 @@
          @endif
         
         <title>{{ config('app.name') }}</title>
-        <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css') }}">
         <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-        <link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css') }}">
-        @toastr_css
+        {{-- @toastr_css --}}
         @stack('css')
         @stack('js-top')
     </head>
@@ -34,8 +33,8 @@
             @include('layouts.footer')
         </div>
         <script src="{{ mix('js/app.js') }}"></script>
-        @stack('js')
-        @toastr_js
+        {{-- @toastr_js --}}
         @toastr_render
+        @stack('js')
     </body>
 </html>
