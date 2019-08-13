9f948c11042aab8e6b27cbd4cd01b7e7849e9b36
winnie <winnie131212592@gmail.com>
2019-08-13 21:58:28 +0800

    separate request concern

--- app/Http/Controllers/ContactFormSubmitController.php
+++ app/Http/Controllers/ContactFormSubmitController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactUsRequest;
 use App\Notifications\ContactUs;
 use App\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Notification;
 
 class ContactFormSubmitController extends Controller
@@ -15,19 +15,8 @@ class ContactFormSubmitController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ContactUsRequest $request)
     {
-        //
-        $request->validate([
-            'name' => 'required|min:3',
-            'email' => 'required|email',
-            'message' => 'required|min:10',
-            'g-recaptcha-response' => 'required|captcha'
-        ],[
-            'g-recaptcha-response.required' => 'Please verify that you are not a robocop :)',
-            'g-recaptcha-response.captcha' => 'Captcha error! try again later or contact site admin.'
-        ]);
-
         Notification::route('mail', config('mail.contact_us'))
             ->notify(new ContactUs(
                 $request->name,
