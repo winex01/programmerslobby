a2d990665910b3e65c157ae4535cd113dfea73f0
winnie <winnie131212592@gmail.com>
2019-08-25 23:20:01 +0800

    fix responsive datatable my projects

--- resources/views/my-projects.blade.php
+++ resources/views/my-projects.blade.php
@@ -29,11 +29,10 @@
 
 
 @push('css')
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.0.3/css/buttons.dataTables.min.css">
 @endpush
 @push('js')
-    {{-- <script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.2/js/jquery.dataTables.js"></script> --}}
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.0.3/js/dataTables.buttons.min.js" defer ></script>
     <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
