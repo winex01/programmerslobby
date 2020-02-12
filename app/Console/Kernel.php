8c9b1d47ca3908449d14bdc8310a53f9c819294a
winex01 <winnie131212592@gmail.com>
2020-02-13 01:38:26 +0800

    Temp config file settings for testing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('backup:run')->everyTenMinutes();   
+        $schedule->command('backup:run')->everyFiveMinutes();   
     }
 
     /**
