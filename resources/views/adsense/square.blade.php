6e4cbd0e005ff0124674b8391109d1adfab81776
winex01 <winnie131212592@gmail.com>
2020-03-12 00:21:02 +0800

    fix square ad

--- resources/views/adsense/square.blade.php
+++ resources/views/adsense/square.blade.php
@@ -1,15 +1,14 @@
-@for($i = 0; $i < 4; $i++)
-	<div class="col-md-6">
-		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-		<!-- square ad -->
-		<ins class="adsbygoogle"
-		     style="display:block"
-		     data-ad-client="ca-pub-5207069007168829"
-		     data-ad-slot="1157426318"
-		     data-ad-format="auto"
-		     data-full-width-responsive="true"></ins>
-		<script>
-		     (adsbygoogle = window.adsbygoogle || []).push({});
-		</script>
-	</div>
-@endfor
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- square ad -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5207069007168829"
+     data-ad-slot="1157426318"
+     data-matched-content-rows-num="2"
+     data-matched-content-columns-num="2"
+     data-matched-content-ui-type="image_stacked"
+     data-ad-format="autorelaxed"
+     data-full-width-responsive="true"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
