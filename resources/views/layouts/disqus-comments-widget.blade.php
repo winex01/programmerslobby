2826023fdd85c5dd73e7fb7c2e0ddb49e0ed2b49
winex01 <winnie131212592@gmail.com>
2020-03-12 01:16:22 +0800

    add new ad above disqus

--- resources/views/layouts/disqus-comments-widget.blade.php
+++ resources/views/layouts/disqus-comments-widget.blade.php
@@ -1,5 +1,11 @@
 <div class="card border-0 shadow mb-4">
 	<div class="card-body">
+		@include('adsense.square')
+	</div>
+</div>
+
+<div class="card border-0 shadow mb-4">
+	<div class="card-body">
 		<div id="disqus_thread"></div>
 	</div>
 </div>
