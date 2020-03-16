fb2b5254cf6729983a1d203da6bff27eef30629c
winex01 <winnie131212592@gmail.com>
2020-03-16 17:01:09 +0800

    added adsense config enabled

---
+++ config/adsense.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    'enabled' => env('ADSENSE_ENABLED', true),
+
+];
--- resources/views/adsense/square.blade.php
+++ resources/views/adsense/square.blade.php
@@ -1,17 +1,18 @@
-<div class="row">
-	<div class="col-md-12">
-		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-		<!-- square ad -->
-		<ins class="adsbygoogle"
-		     style="display:block"
-		     data-ad-client="ca-pub-5207069007168829"
-		     data-ad-slot="1157426318"
-		     data-ad-format="auto"
-		     data-full-width-responsive="true"></ins>
-		<script>
-		     (adsbygoogle = window.adsbygoogle || []).push({});
-		</script>
+@if(config('adsense.enabled'))
+	<div class="row">
+		<div class="col-md-12">
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<!-- square ad -->
+			<ins class="adsbygoogle"
+			     style="display:block"
+			     data-ad-client="ca-pub-5207069007168829"
+			     data-ad-slot="1157426318"
+			     data-ad-format="auto"
+			     data-full-width-responsive="true"></ins>
+			<script>
+			     (adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+		</div>
 	</div>
-</div>
-
-<br>
+	<br>
+@endif
