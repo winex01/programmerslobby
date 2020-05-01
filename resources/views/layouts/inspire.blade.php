651a2da5ee174392c9894867fc0afec6c58fd1c6
winex01 <winnie131212592@gmail.com>
2020-05-01 17:59:42 +0800

    refactor and use programming quote package

--- resources/views/layouts/inspire.blade.php
+++ resources/views/layouts/inspire.blade.php
@@ -1,5 +1,5 @@
 <div class="alert alert-primary alert-dismissible fade show mb-n2" role="alert">
-  <strong>Inspire!</strong> {{ randomQuote() }}
+  <strong>Inspire!</strong> {{ quote_programming() }}
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
