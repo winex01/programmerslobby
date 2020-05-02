a56f5d82500d171a3144beeec2acf0c328330f7a
winex01 <winnie131212592@gmail.com>
2020-05-02 12:03:29 +0800

    fix vultr ads width

--- resources/views/layouts/vultr.blade.php
+++ resources/views/layouts/vultr.blade.php
@@ -1,5 +1,3 @@
-<center>
-    <a href="https://www.vultr.com/?ref=7262203"><img src="https://www.vultr.com/media/banners/banner_468x60.png" width="468" height="60"></a>
-    <a href="https://www.vultr.com/?ref=7262203"><img src="https://www.vultr.com/media/banners/banner_468x60.png" width="468" height="60"></a>
-</center>
-<div class="mb-2"></div>
+<div class="mb-2 text-center">
+	<a href="https://www.vultr.com/?ref=7262203"><img src="https://www.vultr.com/media/banners/banner_728x90.png" width="100%" height="90"></a>
+</div>
