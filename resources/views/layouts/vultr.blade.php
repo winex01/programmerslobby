37633881c97c2ef90e344a0bb25c8aa60936e6da
winex01 <winnie131212592@gmail.com>
2020-05-02 11:17:43 +0800

    add vultr static ads

---
+++ resources/views/layouts/vultr.blade.php
@@ -0,0 +1,5 @@
+<center>
+    <a href="https://www.vultr.com/?ref=7262203"><img src="https://www.vultr.com/media/banners/banner_468x60.png" width="468" height="60"></a>
+    <a href="https://www.vultr.com/?ref=7262203"><img src="https://www.vultr.com/media/banners/banner_468x60.png" width="468" height="60"></a>
+</center>
+<div class="mb-2"></div>
