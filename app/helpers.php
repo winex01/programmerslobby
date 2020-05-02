c6efde70bf4d6c88954b6564647825fbd30e51d0
winex01 <winnie131212592@gmail.com>
2020-05-02 09:20:52 +0800

    refactor helpers and if check

--- app/helpers.php
+++ app/helpers.php
@@ -1,86 +1,94 @@
 <?php 
 
-/**
- * 
- * 
- */
-function widgetButton($string)
-{
-	return 'View all '.strtolower($string);
+
+if (! function_exists('widgetButton')) {
+	/**
+	 * 
+	 */
+	function widgetButton($string)
+	{
+		return 'View all '.strtolower($string);
+	}
 }
 
-/**
- * 
- * 
- */
-function widgetText($string, $count = 0)
-{
-	$string = strtolower($string);
-	return 'You have '.$count.' '.$string.' in your database. Click on button below to view all '.$string.'.';
+
+if (! function_exists('widgetText')) {
+	/**
+	 * 
+	 */
+	function widgetText($string, $count = 0)
+	{
+		$string = strtolower($string);
+		return 'You have '.$count.' '.$string.' in your database. Click on button below to view all '.$string.'.';
+	}
 }
 
-/**
- * 
- * 
- */
-function randomQuote() {
-	return \App\Quotes\Programming::quote();
+
+if (! function_exists('removeWhiteSpaceAndSpecialChars')) {
+	/**
+	 * 
+	 */
+	function removeWhiteSpaceAndSpecialChars($string) {
+
+	    return strip_tags(
+	    	html_entity_decode(
+		    	preg_replace( "/\r|\n/", "", $string)
+		    )
+	    );
+	}
 }
 
-/**
- * 
- * 
- */
-function removeWhiteSpaceAndSpecialChars($string) {
 
-    return strip_tags(
-    	html_entity_decode(
-	    	preg_replace( "/\r|\n/", "", $string)
-	    )
-    );
+if (! function_exists('shortenString')) {
+	/**
+	 * 
+	 */
+	function shortenString($string, $limit = 180) {
+
+		return str_limit(strip_tags($string), $limit);
+	}
 }
 
-/**
- * 
- * 
- */
-function shortenString($string, $limit = 180) {
 
-	return str_limit(strip_tags($string), $limit);
-}
+if (! function_exists('viewIndex')) {
+	/**
+	 * 
+	 */
+	 function viewIndex($folder) {
 
-/**
- * 
- * 
- */
- function viewIndex($folder) {
-
- 	return $folder.'.index';
+	 	return $folder.'.index';
+	 }
  }
 
- /**
- * 
- * 
- */
- function viewShow($folder) {
- 	
- 	return $folder.'.show';
+
+if (! function_exists('viewShow')) {
+	 /**
+	 * 
+	 */
+	 function viewShow($folder) {
+	 	
+	 	return $folder.'.show';
+	 }
  }
 
- /**
- * 
- * 
- */
- function viewCreate($folder) {
- 	
- 	return $folder.'.create';
+
+if (! function_exists('viewCreate')) {
+	 /**
+	 * 
+	 */
+	 function viewCreate($folder) {
+	 	
+	 	return $folder.'.create';
+	 }
  }
 
- /**
- * 
- * 
- */
- function viewEdit($folder) {
- 	
- 	return $folder.'.edit';
+
+if (! function_exists('viewEdit')) {
+	 /**
+	 * 
+	 */
+	 function viewEdit($folder) {
+	 	
+	 	return $folder.'.edit';
+	 }
  }
